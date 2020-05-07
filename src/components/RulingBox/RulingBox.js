ac230abe2c4958e13305555b0ed9032dbfe31c65
Jorge Monroy <jlmonroy13@gmail.com>
2020-05-07 08:39:16 -0500

    Create props to RulingBox component

--- src/components/RulingBox/RulingBox.js
+++ src/components/RulingBox/RulingBox.js
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types';
 
 import Icon from 'components/Icon';
 
-const RulingBox = ({ children }) => (
+const RulingBox = ({ title, description, wikiUrl }) => (
   <div className="ruling-box">
-    {children}
+    <span className="text-small">What's your opinion on</span>
+    <h1 className="ruling-box__title">{title}</h1>
+    <p className="ruling-box__description">{description}</p>
+    <div className="mb-6">
+      <Icon name="wikipedia" className="mr-1" />
+      <a href={wikiUrl} target="_blank" rel="noopener noreferrer" className="ruling-box__link">More information</a>
+    </div>
+    <span className="font-weight-bold text-larger">What's Your Verdict?</span>
     <div className="ruling-box__buttons">
       <button className="ruling-box__button">
         <Icon name="thumb" className="ruling-box__icon ruling-box__icon--up" />
@@ -18,7 +25,9 @@ const RulingBox = ({ children }) => (
 );
 
 RulingBox.propTypes = {
-  children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  wikiUrl: PropTypes.string.isRequired,
 };
 
 export default RulingBox;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,23 +2,17 @@ import React from 'react';
 
 import Header from 'components/Header';
 import RulingBox from 'components/RulingBox';
-import Icon from 'components/Icon';
 
 const Home = () => (
   <div className="position-relative">
     <Header />
     <section className="home__main">
       <div className="container">
-        <RulingBox>
-          <span className="text-small">What's your opinion on</span>
-          <h1 className="home__main-title">Pope Francis?</h1>
-          <p className="home__main-description">He’s talking tough on clergy sexual abuse, but is he just another papal pervert protector? (thumbs down) or a true pedophile punishing pontiff? (thumbs up)</p>
-          <div className="mb-6">
-            <Icon name="wikipedia" className="mr-1" />
-            <a href="https://en.wikipedia.org/wiki/Catholic_Church_sexual_abuse_cases" target="_blank" rel="noopener noreferrer" className="home__main-link">More information</a>
-          </div>
-          <span className="font-weight-bold text-larger">What's Your Verdict?</span>
-        </RulingBox>
+        <RulingBox
+          title="Pope Francis?"
+          description="He’s talking tough on clergy sexual abuse, but is he just another papal pervert protector? (thumbs down) or a true pedophile punishing pontiff? (thumbs up)"
+          wikiUrl="https://en.wikipedia.org/wiki/Catholic_Church_sexual_abuse_cases"
+        />
       </div>
       <div className="home__main-footer">
         <div className="home__main-footer-left">
